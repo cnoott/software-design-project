58cad3db2c40d53f41a8492d23d1db64c18c28a7
Liam Amadio <liam@m1pro.local>
2022-02-17 18:26:01 -0600

    added clinet registration and profile managment templates for rolo and jose

--- sofware-design-frontend/src/RootElement.js
+++ sofware-design-frontend/src/RootElement.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import App from './App';
 import Login from './login/Login';
+import ClientRegistration from './ClientRegistration';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const RootElement = () => {
@@ -9,6 +10,7 @@ const RootElement = () => {
             <Routes>
                 <Route path='/' exact element={<App/>}/>
                 <Route path='/login' exact element={<Login/>}/>
+                <Route path='/client-registration' exact elment={<ClientRegistration/>}/>
             </Routes>
         </BrowserRouter>
     );
