c5cbaff5694cbe3ba18c4bff1a42e9be66c099e0
joselopez117 <joselopez177@gmail.com>
2022-03-04 12:20:23 -0600

    Merge branch 'dev' of https://github.com/cnoott/software-design-project into dev

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -13,7 +13,7 @@ const signup = (req, res) => {
     user.save((err, user) => {
         if (err) {
             return res.status(400).json({
-                error: ''+err
+                error: '' + err
             });
         }
 
@@ -22,9 +22,37 @@ const signup = (req, res) => {
 
         const token = jwt.sign({ _id: user._id, process.env.JWT_SECRET });
 
-        res.cookie('t', token, {expiry: new Date() + 999});
+        res.cookie('t', token, { expiry: new Date() + 999 });
 
         const { _id, username } = user;
-        return res.json({token, user: {_id, username}});
+        return res.json({ token, user: { _id, username } });
+    });
+};
+
+exports.signin = (req, res) => {
+    //find user based on email
+    const { email, password } = req.body;
+    User.findOne({ email }, (err, user) => {
+        if (err || !user) {
+            return res.status(400).json({
+                error: 'User with that email does not exist'
+            });
+        }
+
+        //if user is found make sure email and password match
+        if (!user.authenticate(password)) {
+            return res.status(401).json({
+                error: "Email and password don't match"
+            });
+        }
+
+        //generate signin token with user id and secret
+        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+        //persist token as 't' in cookie with expiry date
+        res.cookie('t', token, { expiry: new Date() + 150 });
+
+        //return response with user and token to frontend client
+        const { _id, name, email, role, profile_picture } = user;
+        return res.json({ token, user: { _id, email, name, role, profile_picture } });
     });
 };
